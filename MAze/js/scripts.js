6d494097de76ce6b5d0b9397ebfbebfcd463e738
Dcorum <dpcorum@yahoo.com>
2018-04-12 11:31:51 -0600

    updated js code with demo code

--- MAze/js/scripts.js
+++ MAze/js/scripts.js
@@ -1,3 +1,73 @@
-window.onload = function(){
+window.onload = function () {
     console.log("JavaScript is working!");
+
+    const SIZE = 4;
+    const CELL_SIZE = 25;
+
+    var selectedX = 0;
+    var selectedy = 0;
+
+    document.onkeydown = function(e){
+        console.log(e.keyCode);
+        if(e.keyCode == 37){
+            console.log("left");
+            selectedX--;
+            redrawGrid();
+        }else if(e.keyCode == 39){
+            console.log("right");
+            selectedX++;
+            redrawGrid();
+        }else if(e.keyCode == 38){
+            console.log("up");
+            selectedy--;
+            redrawGrid();
+        }else if(e.keyCode == 40){
+            console.log("down");
+            selectedy++;
+            redrawGrid();
+        }
+    }
+
+    var myGrid = [];
+
+
+    for (var y = 0; y < SIZE; y++) {
+        myGrid[y] = [];
+        for (var x = 0; x < SIZE; x++) {
+            myGrid[y][x] = y + " , " + x;
+        }
+    }
+
+    console.log(myGrid);
+
+    function redrawGrid(){
+
+        var gridDiv = document.getElementById("gridDiv");
+        gridDiv.innerHTML = "";
+        
+        
+        for (var y = 0; y < SIZE; y++) {
+            var newRow = document.createElement("div");
+            for (var x = 0; x < SIZE; x++) {
+                var newCell = document.createElement("div");
+                
+                newCell.style.height = CELL_SIZE+"px";
+                newCell.style.width = CELL_SIZE+"px";
+                newCell.style.backgroundColor = "silver";
+                newCell.style.display = "inline-block"
+                newCell.style.border = "solid 1px black";
+                
+                newCell.textContent = y + "," + x;
+
+                if(selectedX == x && selectedy == y){
+                    newCell.style.border = "solid 1px red";
+                }
+                
+                newRow.appendChild(newCell);
+            }
+            gridDiv.appendChild(newRow);
+        }
+        
+    }
+    redrawGrid();
 }
