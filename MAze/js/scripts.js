74208322e544fd6fda39d3552e86df0f67a82d52
Dcorum <dpcorum@yahoo.com>
2018-04-12 13:09:43 -0600

    commented out previous code to make use of canvas code and created POC canvas

--- MAze/js/scripts.js
+++ MAze/js/scripts.js
@@ -1,44 +1,55 @@
 window.onload = function () {
     console.log("JavaScript is working!");
 
-    const SIZE = 4;
+    var canvas = document.getElementById("mazecanvas");
+    var ctx = canvas.getContext("2d");
+    // ctx.fillStyle = "black";
+    // ctx.fillRect(0,0,500,500);
+
+    // const SIZE = prompt('Enter the maze size');
+    // if(SIZE == null || SIZE == ""){
+    //     SIZE = 4;
+    // }else{
+    
+    // }
+    SIZE = 4;
     const CELL_SIZE = 25;
 
-    var selectedX = 0;
-    var selectedy = 0;
+    // var selectedX = 0;
+    // var selectedy = 0;
 
-    document.onkeydown = function(e){
-        console.log(e.keyCode);
-        if(e.keyCode == 37){
-            console.log("left");
-            selectedX--;
-            if(selectedX<0){
-                selectedX++;
-            }
-            redrawGrid();
-        }else if(e.keyCode == 39){
-            console.log("right");
-            selectedX++;
-            if(selectedX>SIZE-1){
-                selectedX--;
-            }
-            redrawGrid();
-        }else if(e.keyCode == 38){
-            console.log("up");
-            selectedy--;
-            if(selectedy<0){
-                selectedy++;
-            }
-            redrawGrid();
-        }else if(e.keyCode == 40){
-            console.log("down");
-            selectedy++;
-            if(selectedy>SIZE-1){
-                selectedy--;
-            }
-            redrawGrid();
-        }
-    }
+    // document.onkeydown = function(e){
+    //     console.log(e.keyCode);
+    //     if(e.keyCode == 37){
+    //         console.log("left");
+    //         selectedX--;
+    //         if(selectedX<0){
+    //             selectedX++;
+    //         }
+    //         redrawGrid();
+    //     }else if(e.keyCode == 39){
+    //         console.log("right");
+    //         selectedX++;
+    //         if(selectedX>SIZE-1){
+    //             selectedX--;
+    //         }
+    //         redrawGrid();
+    //     }else if(e.keyCode == 38){
+    //         console.log("up");
+    //         selectedy--;
+    //         if(selectedy<0){
+    //             selectedy++;
+    //         }
+    //         redrawGrid();
+    //     }else if(e.keyCode == 40){
+    //         console.log("down");
+    //         selectedy++;
+    //         if(selectedy>SIZE-1){
+    //             selectedy--;
+    //         }
+    //         redrawGrid();
+    //     }
+    // }
 
     var myGrid = [];
 
@@ -52,34 +63,34 @@ window.onload = function () {
 
     console.log(myGrid);
 
-    function redrawGrid(){
+    // function redrawGrid(){
 
-        var gridDiv = document.getElementById("gridDiv");
-        gridDiv.innerHTML = "";
+    //     var gridDiv = document.getElementById("gridDiv");
+    //     gridDiv.innerHTML = "";
         
         
-        for (var y = 0; y < SIZE; y++) {
-            var newRow = document.createElement("div");
-            for (var x = 0; x < SIZE; x++) {
-                var newCell = document.createElement("div");
+    //     for (var y = 0; y < SIZE; y++) {
+    //         var newRow = document.createElement("div");
+    //         for (var x = 0; x < SIZE; x++) {
+    //             var newCell = document.createElement("div");
                 
-                newCell.style.height = CELL_SIZE+"px";
-                newCell.style.width = CELL_SIZE+"px";
-                newCell.style.backgroundColor = "silver";
-                newCell.style.display = "inline-block"
-                newCell.style.border = "solid 1px black";
+    //             newCell.style.height = CELL_SIZE+"px";
+    //             newCell.style.width = CELL_SIZE+"px";
+    //             newCell.style.backgroundColor = "silver";
+    //             newCell.style.display = "inline-block"
+    //             newCell.style.border = "solid 1px black";
                 
-                newCell.textContent = y + "," + x;
+    //             newCell.textContent = y + "," + x;
 
-                if(selectedX == x && selectedy == y){
-                    newCell.style.border = "solid 1px red";
-                }
+    //             if(selectedX == x && selectedy == y){
+    //                 newCell.style.border = "solid 1px red";
+    //             }
                 
-                newRow.appendChild(newCell);
-            }
-            gridDiv.appendChild(newRow);
-        }
+    //             newRow.appendChild(newCell);
+    //         }
+    //         gridDiv.appendChild(newRow);
+    //     }
         
-    }
-    redrawGrid();
+    // }
+    // redrawGrid();
 }
