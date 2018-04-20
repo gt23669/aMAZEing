068cbf06bffe520dc18c184b4b08300ecc134576
Dcorum <dpcorum@yahoo.com>
2018-04-20 12:06:08 -0600

    corrected eventlistener to execute properly based on button click

--- MAze/js/mazeScript.js
+++ MAze/js/mazeScript.js
@@ -3,9 +3,10 @@ window.onload = function () {
     var canvas = document.getElementById("mazecanvas");
     var ctx = canvas.getContext("2d");
 
+    
     document.onkeydown = function (e) {
         // console.log(e.keyCode);
-        if (e.keyCode == 65 ||e.keyCode == 37) {
+        if (e.keyCode == 65 || e.keyCode == 37) {
             // console.log("left");
             lastPlayerCell.y = playerCell.y;
             lastPlayerCell.x = playerCell.x;
@@ -24,7 +25,7 @@ window.onload = function () {
             ctx.fillStyle = 'red';
             ctx.fillRect(CS * (end.x), CS * (end.y), CS, CS);
             redraw();
-        } else if (e.keyCode == 68||e.keyCode == 39) {
+        } else if (e.keyCode == 68 || e.keyCode == 39) {
             // console.log("right");
             lastPlayerCell.y = playerCell.y;
             lastPlayerCell.x = playerCell.x;
@@ -44,7 +45,7 @@ window.onload = function () {
             ctx.fillRect(CS * (end.x), CS * (end.y), CS, CS);
 
             redraw();
-        } else if (e.keyCode == 87||e.keyCode == 38) {
+        } else if (e.keyCode == 87 || e.keyCode == 38) {
             // console.log("up");
             lastPlayerCell.y = playerCell.y;
             lastPlayerCell.x = playerCell.x;
@@ -64,7 +65,7 @@ window.onload = function () {
             ctx.fillRect(CS * (end.x), CS * (end.y), CS, CS);
 
             redraw();
-        } else if (e.keyCode == 83||e.keyCode == 40) {
+        } else if (e.keyCode == 83 || e.keyCode == 40) {
             // console.log("down");
             lastPlayerCell.y = playerCell.y;
             lastPlayerCell.x = playerCell.x;
@@ -93,7 +94,7 @@ window.onload = function () {
         numRowANDCol = parseInt(numRowANDCol, 10);
         numRowANDCol = numRowANDCol + 1;
         numRowANDCol = String(numRowANDCol);
-        
+
     }
     const rows = numRowANDCol;
     const cols = numRowANDCol;
@@ -111,7 +112,7 @@ window.onload = function () {
     var longestpath = [];
     var playerCell = {};
     var lastPlayerCell = {};
-    
+
     function Coordinate(y, x, direction, visited, distance) {
         this.x = x;
         this.y = y;
@@ -119,7 +120,7 @@ window.onload = function () {
         this.visited = visited;
         this.distance = distance;
     }
-    
+
     function genMazeArr() {
         for (var i = 0; i < cols; i++) {
             gridArr[i] = [];
@@ -132,18 +133,18 @@ window.onload = function () {
         }
         for (var i = 0; i < cols; i++) {
             for (var j = 0; j < rows; j++) {
-                
+
                 if (gridArr[i][j] == 1) {
                     ctx.fillStyle = 'black';
                     ctx.fillRect(CS * i, CS * j, CS, CS);
-                    
+
                 }
             }
         }
     }
-    
+
     genMazeArr();
-    
+
     function selStart() {
         var startx;
         var starty;
@@ -173,9 +174,9 @@ window.onload = function () {
     for (var i = 0; i < cols; i++) {
         // console.log(gridArr[i]);
     }
-    
+
     function path() {
-        
+
         do {
             // console.log("Start: " + start.x, start.y);
             // console.log(trackedPath[0]);
@@ -218,60 +219,61 @@ window.onload = function () {
             // console.log("visitedY: " + [posPath[index].y]);
             cCell.x = posPath[index].x;
             cCell.y = posPath[index].y;
-            
+
             switch (posPath[index].direction) {
                 case "up":
-                gridArr[posPath[index].y + 1][posPath[index].x] = 0;
-                ctx.fillStyle = 'white';
-                ctx.fillRect(CS * (posPath[index].x), CS * (posPath[index].y + 1), CS, CS);
-                // console.log("up")
-                break;
+                    gridArr[posPath[index].y + 1][posPath[index].x] = 0;
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(CS * (posPath[index].x), CS * (posPath[index].y + 1), CS, CS);
+                    // console.log("up")
+                    break;
                 case "down":
-                gridArr[posPath[index].y - 1][posPath[index].x] = 0;
-                ctx.fillStyle = 'white';
-                ctx.fillRect(CS * (posPath[index].x), CS * (posPath[index].y - 1), CS, CS);
-                // console.log("down")
-                break;
+                    gridArr[posPath[index].y - 1][posPath[index].x] = 0;
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(CS * (posPath[index].x), CS * (posPath[index].y - 1), CS, CS);
+                    // console.log("down")
+                    break;
                 case "left":
-                gridArr[posPath[index].y][posPath[index].x + 1] = 0;
-                ctx.fillStyle = 'white';
-                ctx.fillRect(CS * (posPath[index].x + 1), CS * (posPath[index].y), CS, CS);
-                // console.log("left")
-                break;
+                    gridArr[posPath[index].y][posPath[index].x + 1] = 0;
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(CS * (posPath[index].x + 1), CS * (posPath[index].y), CS, CS);
+                    // console.log("left")
+                    break;
                 case "right":
-                gridArr[posPath[index].y][posPath[index].x - 1] = 0;
-                ctx.fillStyle = 'white';
-                ctx.fillRect(CS * (posPath[index].x - 1), CS * (posPath[index].y), CS, CS);
-                // console.log("right")
-                break;
+                    gridArr[posPath[index].y][posPath[index].x - 1] = 0;
+                    ctx.fillStyle = 'white';
+                    ctx.fillRect(CS * (posPath[index].x - 1), CS * (posPath[index].y), CS, CS);
+                    // console.log("right")
+                    break;
             }
             // for (var i = 0; i < cols; i++) {
-                //     console.log(gridArr[i]);
-                // }
-                if (trackedPath[(trackedPath.length) - 1][0] == start.y && trackedPath[(trackedPath.length) - 1][1] == start.x) {
-                    // console.log("true");
-                    break;
-                } else {
-                    // console.log("false");
-                }
-            } while (true);
-            ctx.fillStyle = 'green';
-            ctx.fillRect(CS * (start.x), CS * (start.y), CS, CS);
-            ctx.fillStyle = 'red';
-            ctx.fillRect(CS * (end.x), CS * (end.y), CS, CS);
-            // console.log("exit while loop");
-        }
-        
-        path();
-        
-        redraw();
-        
-        function possPath() {
-            var posPath = [];
-            
-            if (cCell.y - 2 >= 0 && gridArr[cCell.y - 2][cCell.x] == 0) { //up
-                var good = false;
-                for (var i = 0; i < visitedCells.length; i++) {
+            //     console.log(gridArr[i]);
+            // }
+            if (trackedPath[(trackedPath.length) - 1][0] == start.y && trackedPath[(trackedPath.length) - 1][1] == start.x) {
+                // console.log("true");
+                break;
+            } else {
+                // console.log("false");
+            }
+        } while (true);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(CS * (start.x), CS * (start.y), CS, CS);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(CS * (end.x), CS * (end.y), CS, CS);
+        // console.log("exit while loop");
+    }
+
+    path();
+
+    redraw();
+
+    
+    function possPath() {
+        var posPath = [];
+
+        if (cCell.y - 2 >= 0 && gridArr[cCell.y - 2][cCell.x] == 0) { //up
+            var good = false;
+            for (var i = 0; i < visitedCells.length; i++) {
                 if (cCell.y - 2 == visitedCells[i][0] && cCell.x == visitedCells[i][1]) {
                     good = false;
                     break;
@@ -327,39 +329,41 @@ window.onload = function () {
         }
         return posPath;
     }
-    
+
     function redraw() {
         gridArr[start.y][start.x] = 2;
-        
+
         // for(var i = 0;i<numRowANDCol;i++){
-            //     console.log(gridArr[i]);
-            // }
-            for (var j = 0; j < rows; j++) {
-                for (var i = 0; i < cols; i++) {
-                    
-                    if (gridArr[j][i] == 1) {
-                        ctx.fillStyle = 'black';
-                        ctx.fillRect(i * CS, j * CS, CS, CS);
-                    }
+        //     console.log(gridArr[i]);
+        // }
+        for (var j = 0; j < rows; j++) {
+            for (var i = 0; i < cols; i++) {
+
+                if (gridArr[j][i] == 1) {
+                    ctx.fillStyle = 'black';
+                    ctx.fillRect(i * CS, j * CS, CS, CS);
                 }
             }
         }
-        
-        function bot(){
-            
-            // document.getElementById("timer").innerHTML = "Timer: " ;
-            
-            for(var b = 0; longestpath.length > b; b++) {
-                ctx.fillStyle = 'cyan';
-                if(b == 0){
-                    ctx.fillStyle = 'green';
-                ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
-            }if(b == longestpath.length - 1){
-                ctx.fillStyle = 'red';
-            ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
-        }else 
-        ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
     }
+
+    document.getElementById("botButton").addEventListener("click", function(){bot();});
+    
+    function bot() {
+
+        // document.getElementById("timer").innerHTML = "Timer: " ;
+
+        for (var b = 0; longestpath.length > b; b++) {
+            ctx.fillStyle = 'cyan';
+            if (b == 0) {
+                ctx.fillStyle = 'green';
+                ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
+            } if (b == longestpath.length - 1) {
+                ctx.fillStyle = 'red';
+                ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
+            } else
+                ctx.fillRect(longestpath[b][1] * CS, longestpath[b][0] * CS, CS, CS);
         }
-        document.getElementById("botButton").addEventListener("click", bot());
-        }
+    }
+
+}
